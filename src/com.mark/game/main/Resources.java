76fe8b420ff83e1320528bc8866951e8e52ed944
Mark Gerrity <gerrity95@gmail.com>
2017-01-01 21:19:20 +0000

    Added new files and animations for the player for running jumping and ducking.

--- src/com.mark/game/main/Resources.java
+++ src/com.mark/game/main/Resources.java
@@ -15,30 +15,58 @@ import com.mark.framework.animation.Frame;
 
 public class Resources {
 
-    public static BufferedImage welcome, iconimage, selector, bunny1_hurt, bunny1_jump, bunny1_ready, bunny1_walk1, bunny1_walk2, grass, colored_grass_background;
+    public static BufferedImage welcome, iconimage, selector, player_duck, player_jump, player_stand, player_hurt, player_walk2, player_walk7, player_walk6, player_walk5, player_walk4, grass, colored_grass_background;
 
-    public static Animation runAnim;
+    public static Animation runAnim, jumpAnim, duckAnim;
 
     public static void load() {
 
         welcome = loadImage("other/welcome.png");
         iconimage = loadImage("other/iconimage.png");
         selector = loadImage("other/selector.png");
-        bunny1_hurt = loadImage("user/bunny1_hurt.png");
-        bunny1_jump = loadImage("user/bunny1_jump.png");
-        bunny1_ready = loadImage("user/bunny1_ready.png");
-        bunny1_walk1 = loadImage("user/bunny1_walk1.png");
-        bunny1_walk2 = loadImage("user/bunny1_walk2.png");
+
+        //User Images
+        player_duck = loadImage("user/player/p2_duck.png");
+        player_jump = loadImage("user/player/p2_jump.png");
+        player_stand = loadImage("user/player/p2_stand.png");
+        player_hurt = loadImage("user/player/p2_hurt.png");
+
+
+        player_walk2 = loadImage("user/player/player_walk/p2_walk02.png");
+        player_walk7 = loadImage("user/player/player_walk/p2_walk07.png");
+        player_walk6 = loadImage("user/player/player_walk/p2_walk06.png");
+        player_walk5 = loadImage("user/player/player_walk/p2_walk05.png");
+        player_walk4 = loadImage("user/player/player_walk/p2_walk04.png");
+
+
         grass = loadImage("ground/Grass/grass.png");
         colored_grass_background = loadImage("background_1024x640/colored_grass.png");
 
-        Frame f1 = new Frame(bunny1_walk1, .1f);
-        Frame f2 = new Frame(bunny1_walk2, .1f);
+        //Run Frames
+        Frame w1 = new Frame(player_walk2, .1f);
+        Frame w2 = new Frame(player_walk7, .1f);
+        Frame w3 = new Frame(player_walk6, .1f);
+        Frame w4 = new Frame(player_walk5, .1f);
+        Frame w5 = new Frame(player_walk4, .1f);
 
 
+        //Jump Frames:
+        Frame j1 = new Frame(player_walk2, .1f);
+        Frame j2 = new Frame(player_jump, .1f);
 
-        runAnim = new Animation(f1, f2, f1, f2);
+        //Duck Frames
+        Frame d1 = new Frame(player_walk4, .1f);
+        Frame d2 = new Frame(player_duck, .1f);
 
+
+        //Run animation:
+        runAnim = new Animation(w1, w2, w3, w4, w5, w4, w3, w2);
+
+        //Jump Animation:
+        jumpAnim = new Animation(j1, j2);
+
+        //Duck Animation:
+        duckAnim = new Animation(d1, d2);
     }
 
     private static AudioClip loadSound(String filename) {
